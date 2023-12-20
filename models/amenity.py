e652beb932238c07f0997718f4097a70d5b36cc6
Daniel Azil <danielazil.fse@gmail.com>
2023-12-20 22:41:12 +0000

    Modified amenity class to create column of amenity in the database that stores given class attribute data

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,26 @@
-#!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+#!/usr/bin/python
+""" A module that defines the class Amenity and it
+    funtionalities and data attribute to store
+"""
+import models
+from models.base_model import BaseModel, Base
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
-    name = ""
+
+class Amenity(BaseModel, Base):
+    """ A class that sets the coloumn record for the name 
+        of the amenity in the database class attribute
+        for json storage. 
+    """
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """ Constructor for the amenity class """
+        super().__init__(*args, **kwargs)
