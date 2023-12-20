3060730baa4e4ca328a30ceee5facf7bb37ad71a
Daniel Azil <danielazil.fse@gmail.com>
2023-12-20 23:47:07 +0000

    Modified class to create table and column records.

--- models/user.py
+++ models/user.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+""" A module that creates class named User"""
+
+from sqlalchemy.orm import relationship
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+import models
+from models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """ A class inheriting from base model """
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Constructor for the class User"""
+        super().__init__(*args, **kwargs)
