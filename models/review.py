6e8455b4872c9027b05ca8dec273716ea0264d66
Daniel Azil <danielazil.fse@gmail.com>
2023-12-20 23:35:27 +0000

    Modified review class to create table name and column records

--- models/review.py
+++ models/review.py
@@ -1,10 +1,27 @@
-#!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+#!/usr/bin/python
+""" A module that create review class"""
+
+from sqlalchemy import Column, String, ForeignKey
+from os import getenv
+from models.base_model import BaseModel, Base
+import sqlalchemy
+import models
 
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+class Review(BaseModel, Base):
+    """ Initialised class named Review """
+    if models.storage_t == 'db':
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """ Constructor for the class inheriting from the
+            super class constructor
+        """
+        super().__init__(*args, **kwargs)
