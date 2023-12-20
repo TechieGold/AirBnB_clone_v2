12eebe02dd0569703d50543c5e7e29f0d7b4ba7a
Daniel Azil <danielazil.fse@gmail.com>
2023-12-20 14:48:53 +0000

    modified base_model to create records and for time created and updated, as well as the id in the given database.

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,84 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
+"""
+    A module that creates the basemodel to serves as a superclass
+    other subclass that inherits from it.
+"""
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from datetime import datetime
+import sqlalchemy
+import models
+from os import getenv
+
+
+time_utc_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Base = object
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """
+        A module that determines the id and time of which all objects are
+        created and modified.
+    """
+    if models.storage_t == "db":
+        id = Column(String(60), primary_key=True)
+        created_at = Column(DateTime, default=datetime.utcnow)
+        updated_at = Column(DateTime, default=datetime.utcnow)
+
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
+        """
+            initialised the constructor for the base model.
+        """
+        if kwargs:
+            for key_value, obj_data in kwargs.items():
+                if key_value != "__class__":
+                    setattr(self, key_value, obj_data)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time_utc_format)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time_utc_format)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
 
     def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """ A method that displays given content in the specified format"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from models import storage
-        self.updated_at = datetime.now()
-        storage.save()
+        """A method that saves the outlined object created"""
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        """
+            A method that creates a dictionary of given data it is been
+            passed to.
+        """
+        hash_map = self.__dict__.copy()
+        if "created_at" in hash_map:
+            hash_map["created_at"] = hash_map["created_at"].strftime(time_utc_format)
+        if "updated_at" in hash_map:
+            hash_map["updated_at"] = hash_map["updated_at"].strftime(time_utc_format)
+        hash_map["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in hash_map:
+            del hash_map["_sa_instance_state"]
+        return hash_map
+
+    def delete(self):
+        """ A method that deletes given object and it data it contains"""
+        models.storage.delete(self)
