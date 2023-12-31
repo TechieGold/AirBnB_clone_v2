030ddff60429bff68d6eb4297dea14f81886bb82
TechieGold <techiegold101@gmail.com>
2023-12-31 07:20:18 +0100

    update documentation

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# Routes that displays hello HBNB
+# Routes that displays Hello HBNB!
 @app.route('/', strict_slashes=False)
 def hell0():
     return 'Hello HBNB!'
 
 
+# Run the Flask application if this file is executed directly
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
